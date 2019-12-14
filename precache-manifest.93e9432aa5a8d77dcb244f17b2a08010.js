2e2e7ef8242aa874f8007e1541670634c8f66ae1
Zaidi10 <16ume051@lnmiit.ac.in>
2019-12-15 04:54:40 +0530

    Updates

---
+++ precache-manifest.93e9432aa5a8d77dcb244f17b2a08010.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9da9a0730442c083a0ca9b4928527a7",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "41a9f89683924f6ac9f2",
+    "url": "/todolist/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "292545712fa85c18616d",
+    "url": "/todolist/static/css/main.6f4e9b87.chunk.css"
+  },
+  {
+    "revision": "41a9f89683924f6ac9f2",
+    "url": "/todolist/static/js/2.f8cc9aef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.f8cc9aef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "292545712fa85c18616d",
+    "url": "/todolist/static/js/main.a955d199.chunk.js"
+  },
+  {
+    "revision": "b5631cdcdd14849564a7",
+    "url": "/todolist/static/js/runtime-main.75b5d516.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
